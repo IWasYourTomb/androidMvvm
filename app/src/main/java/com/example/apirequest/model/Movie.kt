3cb9bebdbb62965b04e1ec611cb99dec7a21db89
Матвей Андреевич Ганин <ganin@ao-nk.ru>
2022-09-20 11:54:01 +0300

    Serialized

--- app/src/main/java/com/example/apirequest/model/Movie.kt
+++ app/src/main/java/com/example/apirequest/model/Movie.kt
@@ -1,8 +1,13 @@
 package com.example.apirequest.model
 
+import com.google.gson.annotations.SerializedName
+
 
 data class Movie(
+    @SerializedName("name")
     val name: String,
-    val imageUrl:
-    String, val category: String
+    @SerializedName("imageUrl")
+    val imageUrl: String,
+    @SerializedName("category")
+    val category: String
 )
